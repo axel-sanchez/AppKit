5676f9f204d3162ad26a96f803ad53d221818bd7
Axel <axelsan97@hotmail.com>
2025-04-01 03:57:30 -0300

    first commi

--- UalaApp/app/src/main/java/com/example/ualaapp/data/models/DataCities.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ualaapp.data.models
-
-import com.example.ualaapp.helpers.Constants
-
-data class DataCities(
-    val cities: List<City>? = null,
-    val apiError: Constants.ApiError? = null
-)
--- UnfollowersInstagram2/app/src/main/java/com/example/unfollowersinstagram/helpers/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.unfollowersinstagram.helpers
-
-object Constants {
-    const val USER = "user"
-    const val BASE_URL = "https://instagram243.p.rapidapi.com/"
-
-    const val GET_FOLLOWING = "userfollowing/"
-}
